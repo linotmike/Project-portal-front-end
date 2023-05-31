26c4996a0360c11121c850680269f505cd24775e
bear-muna <bearmuna@gmail.com>
2023-05-31 14:03:34 -0700

    fix navbar structure

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,32 +1,18 @@
 import React from 'react'
 import { NavLink as Link } from "react-router-dom";
-import './style.css'
+import './style.css';
 
 
 
 
 export default function Navbar() {
     return (
-        <nav className="nav-main">
-            <ul  className="nav-bar">
-                <li className="link">
-                    <Link to={{ pathname: "/" }}>Home </Link>
-                </li>
-                <li className="link">
-                    <Link to={{ pathname: "/profile" }}>Profile </Link>
-                </li>
-                <li className="link">
-                    <Link to={{ pathname: "/projects/upload" }}>projects</Link>
-                </li>
-                <li className="link">
-                    <Link to={{ pathname: "/messages" }}>messages </Link>
-                </li>
-                <li className="link">
-                    <Link to={{ pathname: "/signin" }}>signin </Link>
-                </li>
-            </ul>
-
-            <br />
-        </nav>
+        <div className='col-5 d-flex justify-content-around nav-links border'>
+                <Link to={{ pathname: "/" }}>Home </Link>
+                <Link to={{ pathname: "/profile" }}>Profile </Link>
+                <Link to={{ pathname: "/projects/upload" }}>projects</Link>
+                <Link to={{ pathname: "/messages" }}>messages </Link>
+                <Link to={{ pathname: "/signin" }}>signin </Link>
+        </div>
     );
 }
