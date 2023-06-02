ab24bf0d787322f4958231356a589d46d4372398
Anthony <anthonyguerrero62@outlook.com>
2023-06-02 14:47:59 -0700

    added send message functionality

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,10 +1,25 @@
 import React from "react";
+import io from 'socket.io-client';
+import { useEffect } from "react"
 
-export default function Home() {
+const socket = io.connect("http://localhost:3001")
+
+export default function Messages() {
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message: "hello" });
+  };
+
+  useEffect(() =>{
+    socket.on("receive_message", (data) => {
+      alert(data.message);
+    })
+  }, [socket])
+
   return (
     <section>
       <input placeholder="message..."></input>
-      <button>Send Message</button>
+      <button onClick={sendMessage}>Send Message</button>
     </section>
   );
 }
