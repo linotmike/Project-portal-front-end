c28567198b4e608ebc694f6383ea477173e5def4
bear-muna <bearmuna@gmail.com>
2023-06-04 00:19:16 -0700

    created some todos to switch search params

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,13 +1,27 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import SearchForm from "../../components/SearchForm";
 import ProjectCard from "../../components/ProjectCard";
 
 export default function Home() {
   const [result, setResult] = useState([]);
+  let queryType = useRef('LANGUAGE');
+
+
+  // TODO: Switch Button Name on click
+  const handleClick = (e) => {
+    e.preventDefault();
+    queryType === 'LANGUAGE' ? queryType = 'NAME' : queryType = 'LANGUAGE';
+    console.log("Current:");
+    console.log(queryType);
+  }
+
   console.log(result);
+
+  // TODO: Switch SearchForm type when clicking button
   return (
     <div className="container-fluid p-3">
+      <button onClick={handleClick}>Search by {queryType.current}</button>
       <SearchForm result={setResult}/>
       <div className='row'>
         { result ?
