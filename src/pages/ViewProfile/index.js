02f2a3f6bf281c01674418d3cc55b232dbe8f933
bear-muna <bearmuna@gmail.com>
2023-06-05 14:56:04 -0700

    created outline for profile fetch

--- src/pages/ViewProfile/index.js
+++ src/pages/ViewProfile/index.js
@@ -1,6 +1,39 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import API from '../../utils/Api';
+import './style.css';
 
 export default function ViewProfile() {
+    const Navigate = useNavigate();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [bio, setBio] = useState('');
+    const [picture, setPicture] = useState('');
+    const [bestWorks, setBestWorks] = useState([]);
+    const [bestWorksOne, setBestWorksOne] = useState('');
+    const [bestWorksTwo, setBestWorksTwo] = useState('');
+    const [bestWorksThree, setBestWorksThree] = useState('');
     
+    const getProfile = async () => {
+        try {
+            const z = await API.getProfile(userId);
+            setPicture(z.Profile.picture);
+            setFirstName(z.Profile.firstName);
+            setLastName(z.Profile.lastName);
+            setBio(z.Profile.bio);
+            setBestWorks(z.Profile.bestWorks.split(' '));
+            setBestWorksOne(bestWorks[0]);
+            setBestWorksTwo(bestWorks[1]);
+            setBestWorksThree(bestWorks[2]);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect( () => {
+        getProfile();
+    }, [userId]);
+
     return 
 }
