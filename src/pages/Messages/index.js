218628fcb4af9b854e5898508bb9302e5af45ee2
Amman Nega <ammannega@gmail.com>
2023-06-04 19:05:10 -0700

    added display of all user projects under the messages tab

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,25 +1,47 @@
 import React from "react";
 import io from 'socket.io-client';
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
+import API from "../../utils/Api";
 
 const socket = io.connect("http://localhost:3001")
 
-export default function Messages() {
+export default function Messages(props) {
+  const [projects, setProjects] = useState([]); // holds state of current user's projects
 
-  const sendMessage = () => {
-    socket.emit("send_message", { message: "hello" });
-  };
 
-  useEffect(() =>{
-    socket.on("receive_message", (data) => {
-      alert(data.message);
-    })
-  }, [socket])
+  useEffect(() => {
+    API.getProjectsByUser(props.userId)
+      .then((data) => {
+        console.log(data);
+        setProjects(data); // set projects to our array
+      }).catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  // const sendMessage = () => {
+  //   socket.emit("send_message", { message: "hello" });
+  // };
+
+  // useEffect(() =>{
+  //   socket.on("receive_message", (data) => {
+  //     alert(data.message);
+  //   })
+  // }, [socket])
 
   return (
-    <section>
-      <input placeholder="message..."></input>
-      <button onClick={sendMessage}>Send Message</button>
-    </section>
+    <div>
+      <section>
+        {projects.map((project) => (
+          <div key={project.id}>
+            <h3>{project.name}</h3>
+          </div>
+        ))}
+      </section>
+      {/* <section>
+        <input placeholder="message..."></input>
+        <button onClick={sendMessage}>Send Message</button>
+      </section> */}
+    </div>
   );
 }
