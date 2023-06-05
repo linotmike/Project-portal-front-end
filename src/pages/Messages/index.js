d9d0e7336995a848df28d678de3b52fb78b98940
Amman Nega <ammannega@gmail.com>
2023-06-04 23:17:09 -0700

    added room configuration based on project

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -7,41 +7,71 @@ const socket = io.connect("http://localhost:3001")
 
 export default function Messages(props) {
   const [projects, setProjects] = useState([]); // holds state of current user's projects
+  
+  // Room State
+  const [room, setRoom] = useState("");
 
+  console.log("Room:",room);
+
+  // Message States
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const joinRoom = (roomNumber) => {
+    if(room !== "") {
+      socket.emit("join_room", roomNumber);
+    }
+  };
+
+  // pass project id as the room number
+  const assignRoom = (roomNumber) => {
+    setRoom(roomNumber);
+    console.log("Room number:",room);
+    joinRoom(roomNumber);
+  };
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
 
   useEffect(() => {
     API.getProjectsByUser(props.userId)
       .then((data) => {
-        console.log(data);
+        console.log("Fetched projects:", data );
         setProjects(data); // set projects to our array
       }).catch((error) => {
         console.error(error);
       });
   }, []);
 
-  // const sendMessage = () => {
-  //   socket.emit("send_message", { message: "hello" });
-  // };
-
-  // useEffect(() =>{
-  //   socket.on("receive_message", (data) => {
-  //     alert(data.message);
-  //   })
-  // }, [socket])
-
   return (
     <div>
       <section>
         {projects.map((project) => (
           <div key={project.id}>
-            <h3>{project.name}</h3>
+            <button onClick={() => assignRoom(project.id)}>{project.name}</button>
           </div>
         ))}
       </section>
-      {/* <section>
-        <input placeholder="message..."></input>
-        <button onClick={sendMessage}>Send Message</button>
-      </section> */}
+      <section>
+        {room !== "" && (
+          <>
+            <input 
+              placeholder="message..." 
+              onChange={(event) => setMessage(event.target.value)}
+            />
+            <button onClick={sendMessage}>Send Message</button>
+          </>
+        )}
+        <h1> Message: </h1>
+        {messageReceived}
+      </section>
     </div>
   );
 }
