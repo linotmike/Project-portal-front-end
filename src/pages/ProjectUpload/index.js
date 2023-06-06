6cb9ee9296c25583ba178fcb349389ab0f9d9b2f
bear-muna <bearmuna@gmail.com>
2023-06-05 22:45:08 -0700

    create project form styled

--- src/pages/ProjectUpload/index.js
+++ src/pages/ProjectUpload/index.js
@@ -7,7 +7,7 @@ import './style.css';
 export default function ProjectUpload({ userId }) {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
-    const [capacity, setCapacity] = useState(0);
+    const [capacity, setCapacity] = useState();
     const [dueDate, setDueDate] = useState(undefined);
     const [languages, setLanguages] = useState([]);
 
@@ -65,7 +65,7 @@ export default function ProjectUpload({ userId }) {
 
       setName('');
       setDescription('');
-      setCapacity(0);
+      setCapacity();
       setDueDate(undefined);
       setLanguages([]);
     }
@@ -73,35 +73,21 @@ export default function ProjectUpload({ userId }) {
     
     return (
         <form className='project-form' onSubmit={handleSubmit}>
-          <div className='row d-flex justify-content-center align-items-center p-2 m-2 border'>
-            <div className='col-md-8 col-12 d-flex flex-column justify-content-evenly align-items-center p-2 border'>
-              <div className='col-10 d-flex flex-column justify-content-evenly align-items-center p-2 m-2 border'>
-                <label for='name'>Name:</label>
-                <input name='name' type='text' placeholder='Project name' value={name} onChange={handleChange}/>
-              </div>
-              <div className='col-10 d-flex justify-content-evenly align-items-center p-2 m-2 border'>
-                <div className='d-flex flex-column justify-content-evenly align-items-center p-2 m-2 border'>
-                  <label for='description'>Description:</label>
-                  <input name='description' type='text' placeholder='Project description' value={description} onChange={handleChange} />
+          <div className='row d-flex justify-content-center align-items-center p-2 m-2'>
+            <div className='col-md-8 col-12 d-flex flex-column justify-content-evenly align-items-center project-create-container p-3 m-3 border'>
+              <h2 className='p-2 m-2'>Create a project</h2>
+              <div className='col-md-10 d-flex justify-content-evenly align-items-center flex-wrap p-2 m-2 border'>
+                <div className='col-12 d-flex flex-column justify-content-between align-items-center left-column p-2 m-2 border'>
+                  <input className='project-create-col p-1' name='name' type='text' placeholder='Name' value={name} onChange={handleChange}/>
+                  <input className='project-create-col p-1' name='capacity' type='number' placeholder='Capacity' value={capacity} onChange={handleChange} />
+                  <input className='project-create-col p-1' name='languages' type='text' placeholder='Languages' value={languages} onChange={handleChange} />
                 </div>
-                <div className='d-flex flex-column justify-content-evenly align-items-center p-2 m-2 border'>
-                  <div className='d-flex flex-column justify-content-evenly align-items-center p-2 m-2 border'>
-                    <label for='capacity'>Capacity:</label>
-                    <input name='capacity' type='number' placeholder='Capacity' value={capacity} onChange={handleChange} />
-                  </div>
-                  <div className='d-flex flex-column justify-content-evenly align-items-center p-2 m-2 border'>
-                    <label for='due-date'>Due date:</label>
-                    <input name='due-date' type='date' placeholder='Date' value={dueDate} onChange={handleChange}/>
-                  </div>
+                <div className='col-12 d-flex flex-column justify-content-evenly align-items-center right-column p-2 m-2 border'>
+                  <textarea className='project-create-col p-1' name='description' type='text' placeholder='Description' value={description} onChange={handleChange} />
+                  <input className='project-create-col p-1' name='due-date' type='date' placeholder='Date' value={dueDate} onChange={handleChange}/>
                 </div>
               </div>
-              <div className='col-10 d-flex flex-column justify-content-evenly align-items-center p-2 m-2 border'>
-                <label for='languages'>Languages:</label>
-                <input name='languages' type='text' placeholder='languages' value={languages} onChange={handleChange} />
-              </div>
-              <div className='p-2 m-2'>
-                <button type='submit'>Create</button>
-              </div>
+              <button className='project-create-btn' type='submit'>Create</button>
             </div>
           </div>
         </form>
