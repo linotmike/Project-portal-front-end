b2cc9a0935e06c89a07cecbee5a87e98ba664fcd
bear-muna <bearmuna@gmail.com>
2023-06-05 22:06:11 -0700

    created structure styling

--- src/pages/ProjectUpload/index.js
+++ src/pages/ProjectUpload/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import API from '../../utils/Api';
+import './style.css';
 
 // TODO: Need to add UserId into object when sending fetch request
 export default function ProjectUpload({ userId }) {
@@ -71,43 +72,38 @@ export default function ProjectUpload({ userId }) {
     
     
     return (
-        <div className='container-fluid p-3'>
-            <div className='row'>
-                <form className='project-form' onSubmit={handleSubmit}>
-                    <div>
-                      <label for='name'>Name:</label>
-                      <input name='name' type='text' placeholder='Project name' value={name} onChange={handleChange}/>
-                    </div>
-                    <div>
-                      <label for='description'>Description:</label>
-                      <input name='description' type='text' placeholder='Project description' value={description} onChange={handleChange} />
-                    </div>
-                    <div>
-                      <label for='capacity'>Capacity:</label>
-                      <input name='capacity' type='number' placeholder='Capacity' value={capacity} onChange={handleChange} />
-                    </div>
-                    <div>
-                      <label for='due-date'>Due date:</label>
-                      <input name='due-date' type='date' placeholder='Date' value={dueDate} onChange={handleChange}/>
-                    </div>
-                    {/* TODO: Status field will only show when you edit your project / Default = Open */}
-                    {/* <div>
-                        <label for='status'>Status:</label>
-                        <input name='status' type='radio' id='open' value={status}/>
-                        <label for='open'>Open</label>
-                        <br />
-                        <input name='status' type='radio' id='open' value={status}/>
-                        <label for='open'>Closed</label>
-                    </div> */}
-                    <div>
-                      <label for='languages'>Languages:</label>
-                      <input name='languages' type='text' placeholder='languages' value={languages} onChange={handleChange} />
-                    </div>
-                    <div>
-                      <button type='submit'>Create</button>
-                    </div>
-                </form>
+        <form className='project-form' onSubmit={handleSubmit}>
+          <div className='row d-flex justify-content-center align-items-center p-2 m-2 border'>
+            <div className='col-md-8 col-12 d-flex flex-column justify-content-evenly align-items-center p-2 border'>
+              <div className='col-10 d-flex flex-column justify-content-evenly align-items-center p-2 m-2 border'>
+                <label for='name'>Name:</label>
+                <input name='name' type='text' placeholder='Project name' value={name} onChange={handleChange}/>
+              </div>
+              <div className='col-10 d-flex justify-content-evenly align-items-center p-2 m-2 border'>
+                <div className='d-flex flex-column justify-content-evenly align-items-center p-2 m-2 border'>
+                  <label for='description'>Description:</label>
+                  <input name='description' type='text' placeholder='Project description' value={description} onChange={handleChange} />
+                </div>
+                <div className='d-flex flex-column justify-content-evenly align-items-center p-2 m-2 border'>
+                  <div className='d-flex flex-column justify-content-evenly align-items-center p-2 m-2 border'>
+                    <label for='capacity'>Capacity:</label>
+                    <input name='capacity' type='number' placeholder='Capacity' value={capacity} onChange={handleChange} />
+                  </div>
+                  <div className='d-flex flex-column justify-content-evenly align-items-center p-2 m-2 border'>
+                    <label for='due-date'>Due date:</label>
+                    <input name='due-date' type='date' placeholder='Date' value={dueDate} onChange={handleChange}/>
+                  </div>
+                </div>
+              </div>
+              <div className='col-10 d-flex flex-column justify-content-evenly align-items-center p-2 m-2 border'>
+                <label for='languages'>Languages:</label>
+                <input name='languages' type='text' placeholder='languages' value={languages} onChange={handleChange} />
+              </div>
+              <div className='p-2 m-2'>
+                <button type='submit'>Create</button>
+              </div>
             </div>
-        </div>
+          </div>
+        </form>
     )
 }
