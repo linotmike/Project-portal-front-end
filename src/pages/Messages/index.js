c7c788b044bdc0ebe3fe684df8a780902b4d1e9e
bear-muna <bearmuna@gmail.com>
2023-06-07 13:09:32 -0700

    added dayjs to messages

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -3,6 +3,7 @@ import io from 'socket.io-client';
 import { useState, useEffect } from "react"
 import './style.css';
 import API from "../../utils/Api";
+import DayJS from 'react-dayjs';
 
 const URL_PREFIX = "http://localhost:3001"
 // const URL_PREFIX = 'https://projectportal-backend.herokuapp.com';
@@ -21,6 +22,7 @@ export default function Messages(props) {
   const [messages, setMessages] = useState([]);
 
   const [picture, setPicture] = useState('');
+  const [createdAt, setCreatedAt] = useState('');
 
   // communicates with backend to switch rooms
   const joinRoom = (roomNumber) => {
@@ -52,6 +54,11 @@ export default function Messages(props) {
       return;
     }
 
+    // add message to Message table in database
+    API.sendMessage(props.userId, room, message).then((res) => {
+      setCreatedAt(res.createdAt);
+    });
+
     const msgObj = { 
       user_id: props.userId,
       username: props.username,
@@ -60,14 +67,13 @@ export default function Messages(props) {
       picture: props.picture,
 
       // createdAt <-- doesn't need to be passed in as database will automatically add the date
+      createdAt: createdAt,
       project_id: room,
       text: message,
     };
 
     socket.emit("send_message", { msgObj, room }); // send new message to back end
     
-    // add message to Message table in database
-    API.sendMessage(props.userId, room, message);
 
     setMessage(""); // clear input field after sending message
   };
@@ -132,15 +138,20 @@ export default function Messages(props) {
         <h1 className="align-self-center p-2 project-message-room-name">{roomName}</h1>
         <div className="col-12 d-flex flex-column justify-content-evenly project-message-container py-2">
           { messages.map((msg, index) => (
-              <div
-                className={ props.userId === msg.user_id ? "d-flex flex-row-reverse justify-content-end align-self-end h-10 p-2 m-2 project-message-send border"
-                 : "d-flex justify-content-star align-self-start h-10 p-2 m-2 project-message-receive border" }
+              <div className={ props.userId === msg.user_id ? "d-flex flex-row justify-content-end align-self-end h-10 p-2 m-2"
+                : "d-flex flex-row-reverse justify-content-start align-self-start h-10 p-2 m-2" }
                 key={index}>
-                  <img className='message-picture align-self-center border' src={msg.picture} alt='profile-picture'/>
-                  <div className="d-flex flex-column px-2">
-                    <p className="align-self-center m-0">{msg.username}</p>
-                    <p className="m-0">{msg.text}</p>
-                  </div>
+                <DayJS format='hh:mm A'>{msg.createdAt}</DayJS>
+                <div
+                  className={ props.userId === msg.user_id ? "d-flex flex-row-reverse justify-content-end align-self-end h-10 p-2 mx-2 project-message-send border"
+                   : "d-flex justify-content-start align-self-start h-10 p-2 mx-2 project-message-receive border" }
+                  key={index}>
+                    <img className='message-picture align-self-center border' src={msg.picture} alt='profile-picture'/>
+                    <div className="d-flex flex-column px-2">
+                      <p className="align-self-center m-0">{msg.username}</p>
+                      <p className="m-0">{msg.text}</p>
+                    </div>
+                </div>
               </div>
           ))}
         </div>
